edc044b02329485c973b05dfe2aef0a452efb2bc
Oscar Toledo <oscartoledo1799@gmail.com>
2024-01-22 13:34:41 -0500

    Convertions

---
+++ convertion.java
@@ -0,0 +1,10 @@
+class convertion
+{
+    public static void main(String args[])
+    {
+        //char ch = 'a'; //Siempre va con comillas simples
+        char ch = 'a';
+        char result = (char)(ch + 1);
+        System.out.println(result);
+    }
+}
