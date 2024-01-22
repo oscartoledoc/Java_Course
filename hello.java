750e394a2e6345825cd2b7aa9ea7ce8e1de2254b
Oscar Toledo <oscartoledo1799@gmail.com>
2024-01-22 13:34:01 -0500

    Advanced lessons

--- hello.java
+++ hello.java
@@ -3,5 +3,6 @@ class hello
     public static void main(String[] args) 
     {
         System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
---
+++ variable.java
@@ -0,0 +1,18 @@
+class variable
+{
+    public static void main(String[] args)
+    {
+        int num1=4;
+        byte by=127;
+        long l=584l;
+
+        float f=5.8f;
+        double d=5.8;
+
+        char c='9';
+
+        boolean b=true;
+
+        System.out.println(c);
+    }
+}
