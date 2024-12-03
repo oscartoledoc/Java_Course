0849c7f642692c697551aad02c53d66b28289fc3
Oscar Toledo <oscartoledo1799@gmail.com>
2024-12-02 20:22:21 -0500

    Update

---
+++ lessonsJava/dynamic_method.java
@@ -0,0 +1,18 @@
+class Payments {
+    void pay(){
+        System.out.println("Este método es para pagar");
+    }
+}
+
+class Tarjeta extends Payments{
+    void pay(){
+        System.out.println("Ingrese el número de tarjeta");
+    }
+}
+
+public class dynamic_method {
+
+    void sound(){
+
+    }
+}
